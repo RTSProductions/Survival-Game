6673433f7bdc258da224795bd68aadbe6ae0822d
RTSProductions <hurleydavidson@gmail.com>
2021-06-09 14:47:44 -0600

    inital commit

---
+++ Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class MeshGenerator : MonoBehaviour
+{
+    Mesh mesh;
+    Vector3[] vertices;
+    Color[] colors;
+    int[] triangles;
+    public Gradient gradient;
+
+    public int xSize = 20;
+
+    public int zSize = 20;
+
+    float minHeight;
+
+    float maxHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = new Mesh();
+
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        if (TryGetComponent<MeshCollider>(out MeshCollider meshCollider))
+        {
+            meshCollider.sharedMesh = mesh;
+        }
+
+        CreatShape();
+
+        UpdatdMesh();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void UpdateColors()
+    {
+        colors = new Color[vertices.Length];
+
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                float height = Mathf.InverseLerp(minHeight, maxHeight, vertices[i].y);
+                colors[i] = gradient.Evaluate(height);
+                i++;
+            }
+        }
+    }
+    void CreatShape()
+    {
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                float y = Mathf.PerlinNoise(x * .3f, z * .3f) * 2;
+                vertices[i] = new Vector3(x, y, z);
+                i++;
+                if (y > maxHeight)
+                {
+                    maxHeight = y;
+                }
+                if (y < minHeight)
+                {
+                    minHeight = y;
+                }
+            }
+        }
+
+        int vert = 0;
+
+        int tris = 0;
+
+        triangles = new int[xSize * zSize * 6];
+
+        for (int z = 0; z < zSize; z++)
+        {
+
+            for (int x = 0; x < xSize; x++)
+            {
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+                vert++;
+                tris += 6;
+            }
+            vert++;
+        }
+    }
+
+    void UpdatdMesh()
+    {
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+
+        mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
+
+        mesh.colors = colors;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices != null)
+        {
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                Gizmos.color = Color.red;
+
+                Gizmos.DrawSphere(vertices[i], .1f);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Noise.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public static class Noise
+{
+    public static float[,] GerenateNoiseMap(int mapWidth, int mapHeight, float scale)
+    {
+        float[,] noiseMap = new float[mapHeight, mapWidth];
+
+        if (scale <= 0)
+        {
+            scale = 0.0001f;
+        }
+
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                float sapmleY = y / scale;
+                float sampleX = x / scale;
+
+                float perlinValue = Mathf.PerlinNoise(sampleX, sapmleY);
+                noiseMap[x, y] = perlinValue;
+            }
+        }
+        return noiseMap;
+    }
+}
